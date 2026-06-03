94d6f4043c74d0430bb431fee533aa6a2e234f0f
Dav2dAnderson <dav2danderson@gmail.com>
2026-06-03 21:55:40 +0500

    updated code

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "studico.onrender.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
