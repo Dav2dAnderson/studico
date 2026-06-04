6b796d8311e5555d19c018f2501e35d497f547da
Dav2dAnderson <dav2danderson@gmail.com>
2026-06-04 07:49:46 +0500

    upgrade next

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bedyM9juPk8q6ApCTeZbXH/4p+dmlSYB7pAqmhi7K/4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
